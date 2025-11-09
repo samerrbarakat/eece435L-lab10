344410145f67f4352393e81f45d4444ae32ba15e
samerrbarakat <barakatsamer932@gmail.com>
2025-11-09 19:21:36 +0200

    Add Jenkins and pipeline setup

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+import unittest 
+from app import greet
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World fromFirstName LastName!")
+        
+if __name__ == "__main__":
+    unittest.main()
